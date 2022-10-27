3e0a1d17a4cb0cd56e597d8b6ab2dceda74e8950
Daniel McGrane <daniel.mcgrane@zebedee.io>
2022-10-27 21:02:36 +0100

    update function

--- netlify/functions/validate-ln-address/validate-ln-address.ts
+++ netlify/functions/validate-ln-address/validate-ln-address.ts
@@ -3,15 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 import axios from "axios";
 
 const handler: Handler = async (event, context) => {
-  const supabaseClient = createClient(
-    process.env.REACT_APP_SUPABASE_URL ?? "",
-    process.env.REACT_APP_SUPABASE_KEY ?? ""
-  );
-  if (
-    event?.queryStringParameters?.lnAddress &&
-    event?.queryStringParameters?.id
-  ) {
-    const { lnAddress, id } = event?.queryStringParameters;
+  if (event?.queryStringParameters?.lnAddress) {
+    const { lnAddress } = event?.queryStringParameters;
 
     const data = await axios.get(
       `https://api.zebedee.io/v0/ln-address/validate/${lnAddress}`,
@@ -24,15 +17,6 @@ const handler: Handler = async (event, context) => {
     );
 
     console.log({ data });
-
-    if (data) {
-      const response = await supabaseClient
-        .from("profile")
-        .update({ lnAddress })
-        .eq("id", id);
-
-      console.log({ response });
-    }
 
     return {
       statusCode: 200,
