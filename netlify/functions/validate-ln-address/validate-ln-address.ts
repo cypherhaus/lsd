9ce6918fe235fe488573ee19466cc669e28e2f77
Daniel McGrane <daniel.mcgrane@zebedee.io>
2022-10-27 21:08:03 +0100

    fix error

--- netlify/functions/validate-ln-address/validate-ln-address.ts
+++ netlify/functions/validate-ln-address/validate-ln-address.ts
@@ -17,7 +17,7 @@ const handler: Handler = async (event, context) => {
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ data }),
+      body: JSON.stringify(data.data),
     };
   }
 
