914d7bd37897325b5a391e7d053302efceed8c31
Daniel McGrane <daniel.mcgrane@zebedee.io>
2022-11-02 20:58:33 +0000

    try movin from option

--- netlify/functions/create-charge/create-charge.ts
+++ netlify/functions/create-charge/create-charge.ts
@@ -37,7 +37,7 @@ const handler: Handler = async (event, context) => {
     };
   }
 
-  console.log({ event: event });
+  console.log({ event: event, clientContext: context.clientContext, context });
   // const decoded = jwt.verify(access_token, process.env.JWT_SECRET ?? "");
 
   const { amount, id } = event?.queryStringParameters;
