01b64ef39b78191bcb26e41fdf962b85feb0ef07
Daniel McGrane <daniel.mcgrane@zebedee.io>
2022-11-02 20:28:12 +0000

    log context

--- netlify/functions/create-charge/create-charge.ts
+++ netlify/functions/create-charge/create-charge.ts
@@ -12,7 +12,7 @@ const CORS_HEADERS = {
 };
 
 const handler: Handler = async (event, context) => {
-  console.log({ event });
+  console.log({ event, context });
   // const decoded = jwt.verify(access_token, process.env.JWT_SECRET ?? "");
 
   const supabaseClient = createClient(
