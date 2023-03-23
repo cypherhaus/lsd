d37f686c329c4c0541741c08c7a3c6eff07d009c
Daniel McGrane <daniel.mcgrane@zebedee.io>
2023-03-23 07:15:06 +0000

    uniused modal components

--- src/components/modals/index.tsx
+++ src/components/modals/index.tsx
@@ -2,14 +2,6 @@ import React from "react";
 import RModal from "react-modal";
 import { useStore } from "../../store";
 import { observer } from "mobx-react-lite";
-import {
-  ADD_BLOCK_MODAL,
-  ADD_MODAL,
-  DELETE_MODAL,
-} from "../../constants/modals";
-import { AddShift } from "./AddShift";
-import { AddBlock } from "./AddBlock";
-import { ConfirmationModal } from "./ConfirmationModal";
 
 const customStyles = {
   content: {
@@ -30,12 +22,8 @@ export const Modal = observer(() => {
   return (
     <RModal
       style={customStyles}
-      onRequestClose={modalView.closeModal}
       isOpen={modalView.modalOpen}
       contentLabel="Example Modal"
-    >
-      {modalView.modalType === ADD_MODAL && <AddShift />}
-      {modalView.modalType === ADD_BLOCK_MODAL && <AddBlock />}
-    </RModal>
+    ></RModal>
   );
 });
