3ea9a583afa093322a6cdd6646b71f9f65b76047
Daniel McGrane <dr.mcgrane@gmail.com>
2023-03-28 08:44:51 +0100

    test vercel

--- src/pages/dashboard/another.tsx
+++ src/pages/dashboard/another.tsx
@@ -3,7 +3,7 @@ import { Layout } from "../../components/common/Layout";
 export default function Another() {
   return (
     <Layout>
-      <div>Another Window</div>
+      <div>Another</div>
     </Layout>
   );
 }
